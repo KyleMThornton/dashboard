6b17d5e69d6e7348e98253c1bb6d508cd18a19b4
KyleMThornton <kylemthornton@gmail.com>
2024-01-07 18:01:04 -0800

    Add unfinished accountsCard

---
+++ src/app/components/AccountsCard.tsx
@@ -0,0 +1,19 @@
+import { StatusOnlineIcon } from "@heroicons/react/outline";
+import {
+  Badge,
+  Card,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeaderCell,
+  TableRow,
+  Text,
+  Title,
+} from "@tremor/react";
+
+export default function AccountsCard() {
+    return (
+        <Card></Card>
+    )
+}
