7167dc90dfc7f4dba0dc5dddc45f0b9af1420f93
KyleMThornton <kylemthornton@gmail.com>
2024-01-07 17:42:53 -0800

    Create Navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+                <span className="font-semibold text-xl tracking-tight">Budget Dashboard</span>
+            </div>
+        </nav>
+    )
+}
