603c70b4e70802a13e005fabf5c97b6dd1fd19a5
KyleMThornton <kylemthornton@gmail.com>
2024-01-07 17:54:30 -0800

    Add SpendingCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import BudgetCard from "./components/BudgetCard";
+import SpendingCard from "./components/SpendingCard";
 
 export default function Home() {
   return (
     <main className="flex justify-center items-center">
       <div className="w-2/3 m-10">
         <BudgetCard />
+        <SpendingCard />
       </div>
     </main>
   )
