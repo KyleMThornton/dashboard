32eb6275f9ef3d5faf8456e8b6ffb1e73c99dc27
KyleMThornton <kylemthornton@gmail.com>
2024-01-09 19:15:39 -0800

    Add additional account options

--- src/app/components/AccountsCard.tsx
+++ src/app/components/AccountsCard.tsx
@@ -15,12 +15,12 @@ import {
 export default function AccountsCard() {
     let checkingBalance = 1000000;
     let savingsBalance = 1000000;
-    let creditCardBalance = 1000000;
+    let creditCard1Balance = 1000000;
+    let creditCard2Balance = 1000000;
     let mortgageBalance = 1000000;
     let investmentBalance = 1000000;
     let retirementBalance = 1000000;
     let homeValue = 1000000;
-    let carValue = 1000000;
     let personalLoanBalance = 1000000;
 
     return (
@@ -41,15 +41,98 @@ export default function AccountsCard() {
                                 Checking
                             </Text>
                         </TableCell>
-                        <TableCell>$ 1,000,000.00</TableCell>
+                        <TableCell>$ {checkingBalance.toLocaleString()}</TableCell>
                     </TableRow>
-                    <TableRow>
+                    <TableRow className="hover:bg-slate-100">
                         <TableCell>
                             <Text className="flex items-center">
                                 Savings
                             </Text>
                         </TableCell>
-                        <TableCell>$ 1,000,000.00</TableCell>
+                        <TableCell>$ {savingsBalance.toLocaleString()}</TableCell>
+                    </TableRow>
+                </TableBody>
+            <Subtitle className="mt-3">Credit</Subtitle>
+                <TableHead>
+                    <TableRow>
+                        <TableHeaderCell>Name</TableHeaderCell>
+                        <TableHeaderCell>Balance</TableHeaderCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow className="hover:bg-slate-100">
+                        <TableCell>
+                            <Text className="flex items-center">
+                                Credit Card 1
+                            </Text>
+                        </TableCell>
+                        <TableCell>$ {creditCard1Balance.toLocaleString()}</TableCell>
+                    </TableRow>
+                    <TableRow className="hover:bg-slate-100">
+                        <TableCell>
+                            <Text className="flex items-center">
+                                Credit Card 2
+                            </Text>
+                        </TableCell>
+                        <TableCell>$ {creditCard2Balance.toLocaleString()}</TableCell>
+                    </TableRow>
+                </TableBody>
+            <Subtitle className="mt-3">Investments</Subtitle>
+                <TableHead>
+                    <TableRow>
+                        <TableHeaderCell>Name</TableHeaderCell>
+                        <TableHeaderCell>Balance</TableHeaderCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow className="hover:bg-slate-100">
+                        <TableCell>
+                            <Text className="flex items-center">
+                                Investment Account
+                            </Text>
+                        </TableCell>
+                        <TableCell>$ {investmentBalance.toLocaleString()}</TableCell>
+                    </TableRow>
+                    <TableRow className="hover:bg-slate-100">
+                        <TableCell>
+                            <Text className="flex items-center">
+                                Retirement Account
+                            </Text>
+                        </TableCell>
+                        <TableCell>$ {retirementBalance.toLocaleString()}</TableCell>
+                    </TableRow>
+                    <TableRow className="hover:bg-slate-100">
+                        <TableCell>
+                            <Text className="flex items-center">
+                                House
+                            </Text>
+                        </TableCell>
+                        <TableCell>$ {homeValue.toLocaleString()}</TableCell>
+                    </TableRow>
+                </TableBody>
+                <Subtitle className="mt-3">Loans</Subtitle>
+                <TableHead>
+                    <TableRow>
+                        <TableHeaderCell>Name</TableHeaderCell>
+                        <TableHeaderCell>Balance</TableHeaderCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow className="hover:bg-slate-100">
+                        <TableCell>
+                            <Text className="flex items-center">
+                                Mortgage
+                            </Text>
+                        </TableCell>
+                        <TableCell>$ {mortgageBalance.toLocaleString()}</TableCell>
+                    </TableRow>
+                    <TableRow className="hover:bg-slate-100">
+                        <TableCell>
+                            <Text className="flex items-center">
+                                Personal Loan
+                            </Text>
+                        </TableCell>
+                        <TableCell>$ {personalLoanBalance.toLocaleString()}</TableCell>
                     </TableRow>
                 </TableBody>
             </Table>
