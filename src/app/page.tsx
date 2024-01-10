7449881424392ce771a00200fa36121a50b1a051
KyleMThornton <kylemthornton@gmail.com>
2024-01-10 13:41:40 -0800

    Update columns and spacing for components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,17 @@ import SpendingCard from "./components/SpendingCard";
 export default function Home() {
   return (
     <main className="flex justify-center items-center">
-      <div className="w-2/3 m-10">
-        <BudgetCard />
-        <SpendingCard />
-        <AccountsCard />
-        <NetWorthCard />
+      <div className="w-3/4 m-10 flex lg:flex-row flex-col">
+        <div className="m-4">
+          <BudgetCard />
+          <SpendingCard />
+        </div>
+        <div className="m-4">
+          <AccountsCard />
+        </div>
+        <div className="m-4">
+          <NetWorthCard />
+        </div>
       </div>
     </main>
   )
