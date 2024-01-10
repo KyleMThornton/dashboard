10f82ea833d71c2348d01a47697a8da53afcb6d2
KyleMThornton <kylemthornton@gmail.com>
2024-01-09 18:55:06 -0800

    Create AccountsCard

--- src/app/components/AccountsCard.tsx
+++ src/app/components/AccountsCard.tsx
@@ -1,4 +1,3 @@
-import { StatusOnlineIcon } from "@heroicons/react/outline";
 import {
   Badge,
   Card,
@@ -10,10 +9,50 @@ import {
   TableRow,
   Text,
   Title,
+  Subtitle,
 } from "@tremor/react";
 
 export default function AccountsCard() {
+    let checkingBalance = 1000000;
+    let savingsBalance = 1000000;
+    let creditCardBalance = 1000000;
+    let mortgageBalance = 1000000;
+    let investmentBalance = 1000000;
+    let retirementBalance = 1000000;
+    let homeValue = 1000000;
+    let carValue = 1000000;
+    let personalLoanBalance = 1000000;
+
     return (
-        <Card></Card>
+        <Card className="max-w-md mx-auto" decoration="top" decorationColor="indigo">
+            <Title>Accounts</Title>
+            <Subtitle className="mt-3">Cash</Subtitle>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableHeaderCell>Name</TableHeaderCell>
+                        <TableHeaderCell>Balance</TableHeaderCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow className="hover:bg-slate-100">
+                        <TableCell>
+                            <Text className="flex items-center">
+                                Checking
+                            </Text>
+                        </TableCell>
+                        <TableCell>$ 1,000,000.00</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell>
+                            <Text className="flex items-center">
+                                Savings
+                            </Text>
+                        </TableCell>
+                        <TableCell>$ 1,000,000.00</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </Card>
     )
 }
